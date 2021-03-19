5f2ca379a53f3275ba22c233e287fa63e2edf846
Rangesh6108 <77319427+Rangesh6108@users.noreply.github.com>
2021-03-19 22:10:17 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,24 @@
+class Paper{
+    constructor(x,y){
+        var options={
+            isStatic:false,
+            restilution:0.3,
+            friction:0,
+            density:1.2
+        }
+        this.body = Bodies.circle(x,y,23,options);
+        
+        this.image=loadImage("paper.png");
+        World.add(world,this.body);
+    }
+
+    display()   {
+        push();
+        var pos = this.body.position;
+        translate(pos.x,pos.y);
+        imageMode(CENTER);
+        image(this.image,0,0,70,70);
+        pop();
+}
+
+}
